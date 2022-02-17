278930b56656b3ade7b071de068a279142663887
Yuxiao QIN <yuxiao.qin@nwpu.edu.cn>
2022-02-18 01:54:58 +0800

    WIP

--- src/generic_slc.jl
+++ src/generic_slc.jl
@@ -13,7 +13,16 @@ Base.@kwdef mutable struct GenericSlc  # keyword argument
     cdata_path
 end
 
-function read_cdata!(sar_image::GenericSlc, lines::Int, pixels::Int)::GenericSlc
+function read_cdata(sar_image::GenericSlc, lines::Int, pixels::Int)::Matrix{ComplexF32}
     stream = open(sar_image.cdata_path, "r+")   # default is read-only
     return Mmap.mmap(stream, Matrix{ComplexF32}, (lines,pixels))
+end
+
+function read_cdata!(sar_image::GenericSlc, lines::Int, pixels::Int)::GenericSlc
+    sar_image.cdata = read_cdata(sar_image, lines, pixels)
+    # update the meta as well
+    (sar_image.meta == nothing) && (sar_image.meta = Dict())
+    sar_image.meta["lines"] = lines
+    sar_image.meta["pixels"] = pixels
+    return sar_image
 end
