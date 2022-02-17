529af140b550775a76db0d808d3ba0983fd861dd
Yuxiao QIN <yuxiao.qin@nwpu.edu.cn>
2022-02-17 20:26:28 +0800

    added test folder structure.

---
+++ test/runtest.jl
@@ -0,0 +1,18 @@
+using Spectrogram
+using Test
+
+tests = [  # modules to be testsed
+    "base",
+    "generic_slc",
+    "gf3",
+]
+
+if length(ARGS) > 0  # options to run specific unittests in cli
+    tests = ARGS
+end
+
+for t in tests
+    test_file = "test_$t.jl"
+    printstyled("* $test_file\n", color = :green)
+    include(test_file)
+end
