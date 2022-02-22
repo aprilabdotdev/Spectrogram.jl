30597ecd3ac5142c3f085cb465905c642ddc2d0b
Yuxiao QIN <yuxiao.qin@nwpu.edu.cn>
2022-02-22 20:43:54 +0800

    added the export.jl module.

---
+++ src/export.jl
@@ -0,0 +1,22 @@
+using Images
+using Plots
+# using FFTW
+
+function serialize(cdata, filename::String="exported.png")
+    save(filename, colorview(Gray, 100*abs.(cdata)/maximum(abs.(cdata))))
+    return true
+end
+
+function serialize(cdata, plot_func::Function=heatmap)
+    cdata .|> abs .|> log |> x -> plot_func(x, color=:jet)
+    return true
+end
+
+# gr()
+# plotlyjs() 会把 xy 轴反转过来？
+# FFTW.set_num_threads(1)
+# A = cdata_crop |> transpose |> copy |> x -> fft(x, 1) .|> abs .|> log
+# B = abs.(fft(copy(transpose(cdata_crop)), 1))
+# azimuth_spectrogram(cdata_crop) |> plot
+# range_spectrogram(cdata_crop) |> plot
+
