ced6396d9890fe9116f70e02cc67beff3ebafd22
Yuxiao QIN <yuxiao.qin@nwpu.edu.cn>
2022-02-22 20:42:10 +0800

    refactored generic_slc.jl and unittests.

--- src/generic_slc.jl
+++ src/generic_slc.jl
@@ -1,11 +1,4 @@
-using Base.Threads
-using FFTW
 using Mmap
-using Setfield
-using SignalAnalysis, SignalAnalysis.Units
-
-import Base: +
-
 
 Base.@kwdef mutable struct GenericSlc  # keyword argument
     cdata = nothing
@@ -13,16 +6,16 @@ Base.@kwdef mutable struct GenericSlc  # keyword argument
     cdata_path
 end
 
-function read_cdata(sar_image::GenericSlc, lines::Int, pixels::Int)::Matrix{ComplexF32}
-    stream = open(sar_image.cdata_path, "r+")   # default is read-only
+function read_cdata(generic_slc::GenericSlc, lines::Int, pixels::Int)::Matrix{ComplexF32}
+    stream = open(generic_slc.cdata_path, "r+")   # default is read-only
     return Mmap.mmap(stream, Matrix{ComplexF32}, (lines,pixels))
 end
 
-function read_cdata!(sar_image::GenericSlc, lines::Int, pixels::Int)::GenericSlc
-    sar_image.cdata = read_cdata(sar_image, lines, pixels)
+function read_cdata!(generic_slc::GenericSlc, lines::Int, pixels::Int)::GenericSlc
+    generic_slc.cdata = read_cdata(generic_slc, lines, pixels)
     # update the meta as well
-    (sar_image.meta == nothing) && (sar_image.meta = Dict())
-    sar_image.meta["lines"] = lines
-    sar_image.meta["pixels"] = pixels
-    return sar_image
+    (generic_slc.meta === nothing) && (generic_slc.meta = Dict())
+    generic_slc.meta["lines"] = lines
+    generic_slc.meta["pixels"] = pixels
+    return generic_slc
 end
--- test/test_generic_slc.jl
+++ test/test_generic_slc.jl
@@ -1,33 +1,23 @@
+using Test, Spectrogram
+using Mmap
+
 @testset "generic-slc-read-data-meta" begin
 
     tmpdir = mktempdir()
-    temppath = joinpath(tmpdir, "temporary.tiff")
-    # create dummy data
-    input = rand(Int16, (1000, 2000, 2))
-    # write dummy data to tempdir
-    AG.create(
-        temppath,
-        driver = AG.getdriver("GTiff"),
-        width=1000,
-        height=2000,
-        nbands=2,
-        dtype=Int16
-    ) do dataset
-        AG.write!(dataset, input, [1,2])
-    end
+    temppath = joinpath(tmpdir, "temporary.raw")
+    lines = 2000
+    pixels = 1000
+    input = rand(ComplexF32, (lines, pixels))
+    stream = open(temppath, "w+")
+    write(stream, input)
+    close(stream)
 
-    # intialize GF3
-    testdata = GF3(
-        cdata_path=temppath,
-        meta_path="test/test_data/gf3_test.meta.xml"
-    )
+    # intialize GenericSlc
+    testdata = GenericSlc(cdata_path=temppath)
+    expected_cdata = read_cdata(testdata, lines, pixels)
+    @test expected_cdata == input
+    read_cdata!(testdata, lines, pixels)
+    @test testdata.meta["lines"] == lines
+    @test testdata.meta["pixels"] == pixels
 
-    expected_cdata = read_cdata(testdata)
-    @test expected_cdata == convert(Matrix{ComplexF32}, input[:,:,1] + im * input[:,:,2])
-
-    expected_meta = read_meta(testdata)
-    @test expected_meta["Number_of_lines_original"] == "28312"
-    @test expected_meta["Number_of_pixels_original"] == "17080"
-    @test expected_meta["Product type specifier"] == "GF3"
-    # @test expected_meta["
 end
