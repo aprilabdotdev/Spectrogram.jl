f9cc15bfd3b4c0f6bd2a607fdfef0f0ade14aa31
Yuxiao QIN <yuxiao.qin@nwpu.edu.cn>
2022-03-11 00:02:07 +0800

    improved io performance: preallocate & type stability.

--- src/generic_slc.jl
+++ src/generic_slc.jl
@@ -7,8 +7,30 @@ Base.@kwdef mutable struct GenericSlc  # keyword argument
 end
 
 function read_cdata(generic_slc::GenericSlc, lines::Int, pixels::Int)::Matrix{ComplexF32}
+    """TODO: do not hardcode data format
+    """
     stream = open(generic_slc.cdata_path, "r+")   # default is read-only
-    return Mmap.mmap(stream, Matrix{ComplexF32}, (lines,pixels))
+    # doris: pixel-interleaved complex-short 4-byte 2b/2b by default
+    cdata_tmp = Mmap.mmap(stream, Matrix{Int16}, (pixels*2, lines))
+    cdata_tmp = copy(transpose(cdata_tmp))  # somehow a transpose is needed here for doris file
+    println("Done")
+
+    slc_tmp = (
+        convert(Matrix{Float32}, cdata_tmp[:, 1:2:end]),
+        convert(Matrix{Float32}, cdata_tmp[:, 2:2:end])
+    )
+
+    # pre-allocation is neccessary for large arrays.
+    slc = Matrix{ComplexF32}(undef, lines, pixels)
+    for col in 1:lines
+        if col % 100 == 0
+            println(col)
+        end
+        slc[col, :] = slc_tmp[1][col, :] .+ im * slc_tmp[2][col, :]
+    end
+
+    close(stream)
+    return slc
 end
 
 function read_cdata!(generic_slc::GenericSlc, lines::Int, pixels::Int)::GenericSlc
